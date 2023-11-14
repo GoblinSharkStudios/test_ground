db0d4df02105e417dfaf669e392b578c2adfe344
GoblinSharkStudios <150698429+GoblinSharkStudios@users.noreply.github.com>
2023-11-14 16:26:56 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,19 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 211, 183, 24),
-          title: Text("Beer Counter"),
-        ),
-      ),
+          appBar: AppBar(
+            backgroundColor: Color.fromARGB(255, 211, 183, 24),
+            title: Text("Beer Counter"),
+          ),
+          body: 
+          ),
     );
   }
 }
