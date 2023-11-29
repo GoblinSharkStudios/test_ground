36d662fb6f4f6949be2a62a2ddc1cc688f5a71a4
GoblinSharkStudios <150698429+GoblinSharkStudios@users.noreply.github.com>
2023-11-29 16:27:02 +0100

    Reworked All
    Added propper BG
    /button slider widget
    /glow on button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stepper_counter_swipe/stepper_counter_swipe.dart';
 
 void main() {
   Paint.enableDithering = true;
@@ -20,94 +21,81 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        //vrchni lista
+        //Upper Lista
         appBar: AppBar(
           backgroundColor: const Color.fromARGB(255, 7, 10, 22),
-          title: const Center(child: Text("Beers & Cheers")),
         ),
 
-        // telo
+        // body and background
         body: Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
-                Colors.black,
-                Color(0xff235457),
+                Color.fromARGB(255, 7, 10, 22),
+                Color.fromARGB(255, 1, 1, 1),
               ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
           ),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 211, 183, 24),
-                    elevation: 3,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(32.0)),
-                    minimumSize: const Size(100, 100), //////// HERE
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      pocetPiv++;
-                    });
-                  },
-                  child: Text(
-                    pocetPiv.toString(),
-                    style: TextStyle(
-                      fontSize: (50.0 + pocetPiv),
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(0, 0, 0, 0),
-                    elevation: 3,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(32.0)),
-                    minimumSize: const Size(20, 20), //////// HERE
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      pocetPiv = 0;
-                    });
-                  },
-                  child: const Icon(
-                    Icons.restore,
-                    color: Color.fromARGB(255, 105, 91, 12),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
 
-        // spodni lista
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: const Color.fromARGB(255, 7, 10, 22),
-          items: const [
-            BottomNavigationBarItem(
-              label: ("Settings"),
-              icon: Icon(
-                Icons.home,
-                color: Color.fromARGB(255, 105, 91, 12),
+          //Shadow of slider counter
+          alignment: Alignment.center,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              boxShadow: const [
+                BoxShadow(
+                  color: Color.fromARGB(255, 7, 10, 22),
+                  spreadRadius: 4,
+                  blurRadius: 10,
+                ),
+                BoxShadow(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  spreadRadius: 2,
+                  blurRadius: 18,
+                )
+              ],
+            ),
+            //Body of Shadow of slider counter
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color.fromARGB(255, 13, 19, 43),
+                    spreadRadius: 3,
+                    blurRadius: 2,
+                  ),
+                  //darker body of a body
+                  BoxShadow(
+                    color: Color.fromARGB(255, 3, 5, 10),
+                    spreadRadius: -4,
+                    blurRadius: 15,
+                  )
+                ],
+              ),
+
+              // Counter itself
+              child: StepperSwipe(
+                stepperValue: 2,
+                initialValue: 0,
+                speedTransitionLimitCount: 3, //Trigger count for fast counting
+                onChanged: (int value) => print('new value $value'),
+                firstIncrementDuration: Duration(
+                    milliseconds: 250), //Unit time before fast counting
+                secondIncrementDuration: Duration(
+                    milliseconds: 100), //Unit time during fast counting
+                direction: Axis.horizontal,
+                dragButtonColor: const Color.fromARGB(255, 3, 5, 10),
+                iconsColor: Color.fromARGB(255, 255, 255, 255),
+                counterTextColor: Color.fromARGB(255, 255, 255, 255),
+                maxValue: 1000,
+                minValue: 0,
+                withBackground: false,
               ),
             ),
-            BottomNavigationBarItem(
-              label: ("Reset"),
-              icon: Icon(
-                Icons.restore_from_trash,
-                color: Color.fromARGB(255, 105, 91, 12),
-              ),
-            )
-          ],
-          currentIndex: currentIndex,
-          onTap: (int index) {
-            setState(() {
-              currentIndex = index;
-            });
-          },
+          ),
         ),
       ),
     );
